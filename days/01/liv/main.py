68f86390be1f48e2ab0dada832eb2a8f0d9b1695
justletmeliv <oabwebster@gmail.com>
2024-12-01 18:20:27 +0000

    Day 1 Part 2

--- days/01/liv/main.py
+++ days/01/liv/main.py
@@ -37,40 +37,33 @@ def read_file(file_path: str) -> List[int]:
             print("Char loop List 2: ",list_2)
     print("Final List 1: ",list_1)
     print("Final List 2: ",list_2)
-    list_1.sort()
-    print("Final Sorted List 1: ",list_1)
-    list_2.sort()
-    print("Final Sorted List 2: ",list_2)
+    # list_1.sort()
+    # print("Final Sorted List 1: ",list_1)
+    # list_2.sort()
+    # print("Final Sorted List 2: ",list_2)
     return list_1, list_2
 
-def list_comparison(list_1 = List[int], list_2 = List[int]) -> int:
-    if len(list_1) == len(list_2):
-        print("List lengths match!")
-        difference_sum = 0
+def similarity_scorer(list_1 = List[int], list_2 = List[int]) -> int:
+    similarity_score = 0
+    if len(list_1) != len(list_2):
+        print("List lengths do not match! Error!")
+    else:
+        print("Run similarity scorer!")
         for int in range(len(list_1)):
-            difference = 0
-            print("List 1: ",list_1[int],"List 2: ",list_2[int])
-            if list_1[int] >= list_2[int]:
-                difference = list_1[int] - list_2[int]
-                print("Difference: ",difference)
-                difference_sum = difference_sum + difference
-                print("List 1 value bigger - difference added to sum")
-            elif list_1[int] < list_2[int]:
-                difference = list_2[int] - list_1[int]
-                print("Difference: ",difference)
-                difference_sum = difference_sum + difference
-                print("List 2 value bigger - difference added to sum")
-            else:
-                print("Error")
-        print("Difference sum = ",difference_sum)
-    return difference_sum
+            int_count = list_2.count(list_1[int])
+            print(list_1[int]," appears ",int_count," times in List 2!")
+            similarity = list_1[int]*int_count
+            print("Similarity = ",similarity)
+            similarity_score = similarity_score + similarity
+        print("Similarity score = ",similarity_score)
+    return similarity_score
 
 def main():
     list_1, list_2 = read_file(FILE_PATH)
     print("Main List 1: ",list_1)
     print("Main List 2: ",list_2)
-    run_comparison = list_comparison(list_1,list_2)
-    print(run_comparison)
+    similarity_value = similarity_scorer(list_1,list_2)
+    print(similarity_value)
 
 if __name__ == "__main__":
     main()
