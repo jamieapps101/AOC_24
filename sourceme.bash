6ccc53d1f0a7825c23f09df6d69b37b3c0265788
Jamie Apps <jamieapps101@gmail.com>
2024-12-01 16:20:07 +0000

    add pre-commit support

--- sourceme.bash
+++ sourceme.bash
@@ -4,6 +4,7 @@ if [ ! -d .venv ] ; then
     python3 -m venv .venv
     source .venv/bin/activate
     python3 -m pip install -r requirements.txt
+    pre-commit install
 else
     source .venv/bin/activate
 fi
