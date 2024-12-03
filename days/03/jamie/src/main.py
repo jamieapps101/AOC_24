35900ee7dc6a412e76b5ca128cae9c7a1bbac22f
Jamie Apps <jamieapps101@gmail.com>
2024-12-03 07:34:16 +0000

    day 3 part 2

--- days/03/jamie/src/main.py
+++ days/03/jamie/src/main.py
@@ -5,8 +5,14 @@ import sys
 def main():
     s = sys.stdin.read()
     sum = 0
-    for match in re.finditer(r"mul\(([0-9]+),([0-9]+)\)", s):
-        sum += int(match.group(1)) * int(match.group(2))
+    enabled = True
+    for match in re.finditer(r"mul\(([0-9]+),([0-9]+)\)|do\(\)|don't\(\)", s):
+        if match.group(0) == "do()":
+            enabled = True
+        elif match.group(0) == "don't()":
+            enabled = False
+        elif enabled:
+            sum += int(match.group(1)) * int(match.group(2))
     print(f"{sum=}")
 
 
